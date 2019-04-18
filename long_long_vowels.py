c1b09456325e52e92642e28e366f40d649f236f1
RJ Salamanca <salamancaronito@gmail.com>
2019-04-18 18:42:04 -0400

    added version 2

--- long_long_vowels.py
+++ long_long_vowels.py
@@ -1,7 +1,11 @@
+# Version 1
+
+'''
 word = input('Enter a word: ')
 new_word = ''
 
 for i in range(len(word)):
+    #look for voels only
     if word[i] == 'a' or word[i] == 'e' or word[i] == 'i' or word[i] == 'o' or word[i] == 'u':
         #dont check the last letter because nothing is infront of it.
         if i < (len(word)-1):
@@ -10,5 +14,21 @@ for i in range(len(word)):
                 #only multipied by 3 because the current vowel and the next vowel is added by default
                 new_word += word[i]*3
     new_word += word[i]
+
+print(new_word)
+'''
+
+# Version 2 
+
+word = input('Enter a word: ')
+previous_letter = ''
+new_word = ''
+
+for letter in word:
+    if previous_letter == letter:
+        new_word += letter * 4
+    else:
+        new_word += letter
+    previous_letter = letter
 
 print(new_word)
